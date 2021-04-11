aef24a504dc0f0e67d78edd555a31c8c63240c67
vlshk999 <vlasek.ond@gmail.com>
2021-04-12 00:21:27 +0200

    su moc krasny
    jjj

---
+++ autoservis/application/views/pages/home.php
@@ -0,0 +1,18 @@
+
+
+<H1 class="text-justify text-center" style="margin-top:50px"> AUTOSERVIS </H1>
+<p></p>
+<div class="text-center">
+<a href="https://www.youtube.com/watch?v=3mLOUGLx9d4" target="_blank" ><img  src="https://starecat.com/content/wp-content/uploads/fiat-multipla-after-tuning.jpg"  alt="tuningJakSvine" href="https://www.youtube.com/watch?v=3mLOUGLx9d4">
+</a>
+<H3> STRÁNKA DOSTUPNÁ JIŽ BRZY! OBJEDNÁVKY PŘIJÍMÁME NA 0949 193 503.</H3>
+
+
+
+<div><iframe width="300" height="60" src="https://vocaroo.com/embed/16InHbzcJoLf?;autoplay=1" frameborder="0" allow="autoplay" ></iframe><br></div>
+
+
+
+
+</div>
+
---
+++ autoservis/application/views/templates/footer.php
@@ -0,0 +1,5 @@
+<div class=text-center style="margin-top:50px">
+<em>&copy; 2021</em>
+        </div>
+        </body>
+</html>
